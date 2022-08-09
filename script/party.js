a3baa790ff3375cd2c5994f864ec21b1db71074c
Alex Henry <tukkek@gmail.com>
2022-08-09 18:59:34 -0300

    First version done...
    Hobbit rogue, Dracon fighter and Mook psionic are present in the vast majority of parties.
    That's because the races are mostly one-class ponies and this has a 1 class and 1 race limit per party.
    Trying to relax requirements  (removing them from all roles to promote other heroes) isn't helping.
    The race-class identity is fine and organic, the problem is having 9/10 parties have 3/6 fixed heroes.

--- script/party.js
+++ script/party.js
@@ -1,33 +1,90 @@
 import * as stats from './stats.js'
 import * as rpg from './rpg.js'
 
-const HEALER=[]
-const ROGUE=[]
-const TANK=[]
+const MAGIC={
+  'mental':[stats.psionic,stats.monk,stats.bishop,],
+  'water':[stats.alchemist,stats.bishop,],
+  'divine':[stats.priest,stats.valkyrie,stats.bishop,],
+  'fire':[stats.mage,stats.samurai,stats.bishop,],
+}
+const HEALER=[...new Set(['water','divine','mental'].flatMap(m=>MAGIC[m]))]
+const BNE=[stats.ninja,stats.rogue,stats.bard,stats.gadgeteer]
+const TANK=[stats.fighter,stats.lord,stats.valkyrie,stats.samurai,stats.monk,stats.bard,stats.priest,stats.rogue]
+const CASTER=[stats.bishop,stats.mage,stats.samurai,stats.monk,stats.gadgeteer,stats.bard]
+const MELEE=[stats.fighter,stats.lord,stats.valkyrie,stats.samurai,stats.ninja,stats.monk,stats.rogue,stats.bard,stats.gadgeteer,stats.priest,]
+const SUPPORT=[stats.ranger,stats.samurai,stats.ninja,stats.monk,stats.bard,stats.gadgeteer,stats.priest,stats.alchemist,stats.bishop,stats.psionic,stats.mage]
+const ROLES=[HEALER,TANK,BNE]
+const OPTIONALROLES=[CASTER]
+
+class Hero{
+  constructor(race,profession=false){
+    this.race=race
+    this.profession=profession
+  }
+  
+  toString(){
+    let p=this.profession?this.profession.name:'?'
+    return `${this.race} ${p.toLowerCase()}`
+  }
+  
+  compare(hero){
+    let a=MELEE.indexOf(this.profession)>=0
+    let b=MELEE.indexOf(hero.profession)>=0
+    if(a==b) return 0
+    return a&&!b?-1:+1
+  }
+}
 
 class Party{
   constructor(){
-    this.races=rpg.shuffle(stats.races,true)
-    this.professions=[]
-    this.make()
+    this.heroes=[]
+  }
+  
+  select(role){return this.heroes.find(h=>role.indexOf(h.profession)>=0)}
+  
+  place(position){
+    let placed=this.heroes.filter(h=>position.indexOf(h.profession)>=0)
+    return placed.length>=3&&placed.slice(0,3)
+  }
+  
+  setup(){
+    let chosen=rpg.shuffle(ROLES).map(r=>this.select(r))
+    if(chosen.findIndex(c=>!c)>=0) return false
+    chosen.push(...rpg.shuffle(OPTIONALROLES).map(r=>this.select(r)).filter(r=>r))
+    let melee=this.place(MELEE)
+    let support=this.place(SUPPORT)
+    if(!melee||!support) return false
+    chosen.push(...melee)
+    chosen.push(...support)
+    chosen=new Set(chosen)
+    if(chosen.size>6) return false
+    for(let i=0;chosen.size<6&&i<this.heroes.length;i++)
+      chosen.add(this.heroes[i])
+    if(chosen.size!=6) return false
+    this.heroes=[...chosen]
+    /*for(let i=0;i<=this.professions,length;i++)
+      if(!chosen.has(this.professions[i])){
+        this.professions.splice(i,1)
+        this.races.splice(i,1)
+      }*/
+    this.heroes.sort((a,b)=>a.compare(b))
+    return true
   }
   
   make(){
-    for(let r of Array.from(this.races)){
-      let favored=r.favor()
-      favored=favored.filter(f=>this.professions.indexOf(f)<0)
-      if(favored.length>0) this.professions.push(rpg.pick(favored))
-      else this.races.splice(this.races.indexOf(r),1)
+    this.heroes=rpg.shuffle(stats.races).map(r=>new Hero(r))
+    for(let h of Array.from(this.heroes)){
+      let favored=h.race.favor()
+      favored=favored.filter(f=>this.heroes.map(h=>h.profession).indexOf(f)<0)
+      if(favored.length>0) h.profession=rpg.pick(favored)
+      else this.heroes.splice(this.heroes.indexOf(h)) //this.races.splice(this.races.indexOf(h.race),1)
     }
-    if(this.races.length>6){
-      this.races=this.races.splice(0,6)
-      this.professions=this.professions.splice(0,6)
-    }
+    return this.heroes.length>=6&&this.setup()
   }
 }
 
 export function make(){
-  let party=new Party()
-  console.log(party.races.map(r=>r.name))
-  console.log(party.professions.map(p=>p.name))
+  let p=new Party()
+  while(!p.make()) continue
+  return p
 }
